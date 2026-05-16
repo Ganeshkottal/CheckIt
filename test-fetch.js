e3f188d51c7bd9d65d16f48cb6ecf1036f4b6455
Ganeshkottal <soumyakottal@gmail.com>
2026-05-16 23:56:50 +0530

    Delete test-fetch.js

--- test-fetch.js
+++
@@ -1,8 +0,0 @@
-fetch("https://generativelanguage.googleapis.com")
-    .then(res => {
-        console.log("CONNECTED");
-        console.log("Status:", res.status);
-    })
-    .catch(err => {
-        console.error("FETCH FAILED:", err);
-    });
